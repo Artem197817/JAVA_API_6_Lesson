0eb03384619039f25841b447940bd8803e2e55af
Onna Mcadva <anna.anutka@gmail.com>
2023-08-05 18:09:45 +0200

    6 урок - 2023.08.05 01:21 предпоследняя редакция 2

--- src/main/java/org/example/Notebook.java
+++ src/main/java/org/example/Notebook.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Objects;
-
 public class Notebook {
     private final int ram;
     private final String drive;
@@ -54,19 +52,6 @@ public class Notebook {
                 ", Марка: '" + brand + '\'' +
                 ", Цвет: '" + color + '\'' +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Notebook notebook = (Notebook) o;
-        return ram == notebook.ram && Double.compare(screenSize, notebook.screenSize) == 0 && Objects.equals(drive, notebook.drive) && Objects.equals(brand, notebook.brand) && Objects.equals(os, notebook.os) && Objects.equals(color, notebook.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(ram, drive, screenSize, brand, os, color);
     }
 
 }
