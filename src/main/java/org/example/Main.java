7125ae35f170a1b64567bc28840819bd00770bde
Artem <katod1978@yandex.ru>
2023-08-21 21:04:51 +0700

    Исправлено и прокомментировано

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,16 +23,19 @@ public class Main {
                 break;
             case ("1"):
                 System.out.println("\n*  Ноутбуки в наличии:  *\n");
-                notebookPrint(notebooks);
+                notebooks.forEach(System.out::println); //  зачем тут вообще метод
+                // notebookPrint(notebooks);
                 System.out.printf("\n*  Найдено %d из %d ноутбуков  *\n", totalNotebook, totalNotebook);
                 break;
             case ("2"):
-                Set<Notebook> notebookFromFilter = NoteCustomer.filter(notebooks);
-                int counter = notebookFromFilter.size();
+                NoteCustomer.filter(notebooks);
+                //Set<Notebook> notebookFromFilter = NoteCustomer.filter(notebooks);
+                int counter = NoteCustomer.notebookFilter.size();
                 if (counter == 0) {
                     System.out.println("\n*  Не найдено ни одного ноутбука  *\n");
                 } else {
-                    notebookPrint(notebookFromFilter);
+                    notebookPrint(NoteCustomer.notebookFilter);
+                    // notebookPrint(notebookFromFilter);
                     System.out.printf("\n*  Найдено %d из %d ноутбуков  *\n", counter, totalNotebook);
                  }
                 break;
@@ -46,9 +49,15 @@ public class Main {
      * @apiNote печатает список ноутбуков с параметрами
      * @param notebooks список ноутбуков
      */
-    private static void notebookPrint(Set<Notebook> notebooks) {
+    private static void notebookPrint(List<Notebook> notebooks) {
+        //этот метод лучше разиестить в классе NoteCustomer в классе Main не принято размещать методы хотя он вообще лишний
         for (Notebook notebook : notebooks) {
             System.out.println(notebook);
         }
     }
 }
+// Есть вопросы к логике интерфейса почему бы не распечатать список ноутбуков перед выбором. Попал в какой нибудь выбор только
+// c третьего раза. Логику с 15 по 55 строку лучше размещать в своем классе.
+// И обращаться к ней через методы В классе Main кроме обращения к методам и выводу
+// результатов ничего быть не должно по идеи.
+// очень интересное решение по заполнению массива случайными ноутбуками оценил)).
--- src/main/java/org/example/NoteCustomer.java
+++ src/main/java/org/example/NoteCustomer.java
@@ -1,19 +1,20 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class NoteCustomer {
-    public static Set<Notebook> filter(Set<Notebook> notebooks) {
+    private static int ram;
+    private static double screenSize;
+    private static String os;
+    public static  List<Notebook> notebookFilter = new ArrayList<>();
+    public static void filter(Set<Notebook> notebooks) {
         System.out.println("Выберите Операционную систему, введя первую букву навания");
         System.out.println("W = \"Windows\", U = \"Ubuntu\", F = \"Fedora\", C = \"Chrome OS\", S = \"Solaris\"");
         Scanner scanner = new Scanner(System.in);
-        String os = scanner.nextLine();
+        os = scanner.nextLine();
 
         System.out.println("Введите минимальное значение Оперативной памяти [4 - 32]");
-        int ram;
+
         try {
             ram = scanner.nextInt();
         } catch (InputMismatchException e) {
@@ -23,16 +24,25 @@ public class NoteCustomer {
         }
 
         System.out.println("Введите минимальное значение Размера экрана [11 - 17]");
-        double screenSize;
+
         try {
             screenSize = scanner.nextDouble();
         } catch (InputMismatchException e) {
             //System.out.println("exception caught double");
-            screenSize = 0.0;
+            //screenSize = 0.0;
+            screenSize = 0;
         }
         scanner.close();
 
-        Set<Notebook> notebookFromFilter = new HashSet<>();
+
+
+        notebookFilter = notebooks.stream()
+                .filter(x -> ram <= x.getRam())
+                .filter(x -> os.equalsIgnoreCase(String.valueOf(x.getOs().charAt(0))))
+                .filter(x -> screenSize <= x.getScreenSize())
+                .toList();
+
+       /* Set<Notebook> notebookFromFilter = new HashSet<>();
         for (Notebook notebook : notebooks) {
             if (ram <= notebook.getRam()
                     && os.equalsIgnoreCase(String.valueOf(notebook.getOs().charAt(0)))
@@ -44,11 +54,14 @@ public class NoteCustomer {
                         notebook.getOs(),
                         notebook.getColor()));
             }
-        }
-        return notebookFromFilter;
+        } */
     }
 }
-
+// Наплодила очень много лишних объектов. Зачем?
+// Все переменные используемые в классе принято инициализировать вначале с применением модификаторов доступа.
+// обрати внимание что метод filter в моем исполнении ничего не возврвщает.
+// он создает фильтрованный лист и мы просто обращаемся к нему из других классов напрямую.
+// учи Stream многие вопросы становятся проще))
 
 
 
